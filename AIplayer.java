311283fc21b84f7b78d3195cf2397b1b8f17163e
chintas1 <chintas1@tcnj.edu>
2015-05-04 12:57:03 -0400

    Working Connect 4

--- AIplayer.java
+++ AIplayer.java
@@ -9,21 +9,35 @@ public class AIplayer
 { 
 	
 	Random chooser = new Random();
-	int column;
 	int Color;
 	public AIplayer(int playerColor)
 	{
 		Color = playerColor;
 	}
 	
-	public int makeMove ()
+	public int makeMove (String aMoves)
 	{
 		//all the random player should care about is picking a column to drop in.
-		column = chooser.nextInt(7);
-		column++;
-		System.out.println("I choose column " + column);
-		//Make sure that column isn't filled, if it is, choose again. If there is still space there, make the move
-		return column;
+		int m = genMove();
+		System.out.println(m+"");
+		boolean gen = true;
+		while(gen)
+		{
+			if(aMoves.contains(m+""))
+			{
+				gen = false;
+			}
+			else
+			{
+				m = genMove();
+				System.out.println(m+"");
+			}
+		}
+		return m;
+	}
+	public int genMove()
+	{
+		return chooser.nextInt(7)+1;
 	}
 	public int getRandomPlayerColor()
 	{
--- Connect4.java
+++ Connect4.java
@@ -2,8 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
  
-public class Connect4 
-{
+public class Connect4 {
         public static void main(String[] args) 
        {
                 Connect4UI frame = new Connect4UI();
@@ -14,12 +13,16 @@ public class Connect4
  
 class Connect4UI extends JFrame implements ActionListener 
 {
-        private Button column1, column2, column3, column4, column5, column6, column7;
+        private JButton column1, column2, column3, column4, column5, column6, column7;
         private Label           butPos;
         MenuItem                newGame, exitGame, redPlayer, bluePlayer;
         int[][]                 boardArray;
-        boolean                 gameover=false;
+        boolean                 gameover;
         boolean                 first;
+        boolean			sim = false;
+        int		round=1;
+        int		wins = 0;
+        int		score = 50;
 
         public static final int BLANK = 0;
         public static final int RED = 1;
@@ -55,43 +58,43 @@ class Connect4UI extends JFrame implements ActionListener
  
                 Panel panel = new Panel();
  
-                column1 = new Button("v");
+                column1 = new JButton("v");
                 column1.addActionListener(this);
                 panel.add(column1);
                 butPos = new Label(SPACE);
                 panel.add(butPos);
  
-                column2 = new Button("v");
+                column2 = new JButton("v");
                 column2.addActionListener(this);
                 panel.add(column2);
                 butPos = new Label(SPACE);
                 panel.add(butPos);
  
-                column3 = new Button("v");
+                column3 = new JButton("v");
                 column3.addActionListener(this);
                 panel.add(column3);
                 butPos = new Label(SPACE);
                 panel.add(butPos);
  
-                column4 = new Button("v");
+                column4 = new JButton("v");
                 column4.addActionListener(this);
                 panel.add(column4);
                 butPos = new Label(SPACE);
                 panel.add(butPos);
  
-                column5 = new Button("v");
+                column5 = new JButton("v");
                 column5.addActionListener(this);
                 panel.add(column5);
                 butPos = new Label(SPACE);
                 panel.add(butPos);
  
-                column6 = new Button("v");
+                column6 = new JButton("v");
                 column6.addActionListener(this);
                 panel.add(column6);
                 butPos = new Label(SPACE);
                 panel.add(butPos);
  
-                column7 = new Button("v");
+                column7 = new JButton("v");
                 column7.addActionListener(this);
                 panel.add(column7);
  
@@ -106,7 +109,11 @@ class Connect4UI extends JFrame implements ActionListener
                 for (int row=0; row<MAXROW; row++)
                         for (int col=0; col<MAXCOL; col++)
                                 boardArray[row][col]=BLANK;
-                first=false;
+                first=true;
+               	gameover=false;
+                activeColour = RED;
+        	System.out.println("Next game starting. Round:" + (round) + " " + gameover + " " + activeColour);
+                AIplayer = new AIplayer(BLUE);
         }
  
         public void paint(Graphics g) 
@@ -121,38 +128,55 @@ class Connect4UI extends JFrame implements ActionListener
                                 if (boardArray[row][col]==BLUE) g.setColor(Color.BLUE);
                                 g.fillOval(160+100*col, 100+100*row, 100, 100);
                         }
-                gameStatus(g);
         }
  
         public boolean place(int n) 
        {
-                if (gameover) return false;
+       	        if(sim)
+       	        	return false;
+       	        if(activeColour==BLUE)
+       	        {
+       	        	AIplace(AIplayer.makeMove(availMoves()));
+       	        	return false;
+       	        }
+       	        System.out.println("You choose column " + n);
                 first=true;
                 int row;
                 n--;
                 for (row=0; row<MAXROW; row++)
-                        if (boardArray[row][n]>0) break;
+                        if (n>6 || boardArray[row][n]>0) break;
+                
                 if (row>0) 
                {
                         boardArray[--row][n]=activeColour;
                         if (activeColour==RED)
                                 activeColour=BLUE;
                         else
-                                activeColour=RED;
+                                activeColour=RED;;
                         repaint();
-                        AIplace(AIplayer.makeMove());
+                        gameover = gameStatus();
+                        if(!gameover)
+                        {
+                        	AIplace(AIplayer.makeMove(availMoves()));
+                        }
+                        else
+                        {
+                        	gameover = false;
+                        	return false;
+                        }
                         return true;
                 }
                 return false;
         }
         public boolean AIplace(int n) 
        {
-                if (gameover) return false;
+       	        if (gameover) return false;
                 first=true;
                 int row;
                 n--;
+		System.out.println("I choose column " + (n + 1));
                 for (row=0; row<MAXROW; row++)
-                        if (boardArray[row][n]>0) break;
+                        if (n>6 || boardArray[row][n]>0) break;
                 if (row>0) 
                {
                         boardArray[--row][n]=activeColour;
@@ -161,25 +185,50 @@ class Connect4UI extends JFrame implements ActionListener
                         else
                                 activeColour=RED;
                         repaint();
+                        gameover = gameStatus();
+                        if(gameover)
+                        {
+                        	gameover = false;
+                        	return false;
+                        }
                         return true;
                 }
                 return false;
         }
- 
-        public void displayWinner(Graphics g, int n) 
-       {
-                g.setColor(Color.CYAN);
-                g.setFont(new Font("Courier", Font.BOLD, 100));
-                if (n==RED)
-                        g.drawString("Red wins!", 250, 400);
-                else
-                        g.drawString("Blue wins!", 250, 400);
-                gameover=true;
+        public String availMoves()
+        {
+        	String aMoves = "";
+        	for(int i = 0; i < 7; i++)
+        	{
+        		if(boardArray[0][i]==0)
+        			aMoves = aMoves+(i+1);
+        	}
+        	System.out.println(aMoves);
+        	return aMoves;
         }
- 
-        public void gameStatus(Graphics g) 
+        public void displayWinner(int n) 
        {
-                for (int row=0; row<MAXROW; row++) 
+                gameover=true;
+                if (n==RED && gameover)
+                {
+                	wins++;
+                        JOptionPane.showMessageDialog(this, "You win. Current Score is: " + wins + "-" + (round-wins));
+                        //score calculation code
+                }
+                else if(gameover)
+                {
+                        JOptionPane.showMessageDialog(this, "Computer wins. Current Score is: " + wins + "-" + (round-wins));
+                        //score calculation code
+                }
+                sim = true;
+                column7.doClick();
+                gameover=false;
+        }
+        public boolean gameStatus()
+        {
+        	if(gameover)
+        		return false;
+        	for (int row=0; row<MAXROW; row++) 
                {
                         for (int col=0; col<MAXCOL-3; col++) 
                        {
@@ -188,7 +237,8 @@ class Connect4UI extends JFrame implements ActionListener
                                  && slot == boardArray[row][col+1]
                                  && slot == boardArray[row][col+2]
                                  && slot == boardArray[row][col+3]) {
-                                        displayWinner(g, boardArray[row][col]);
+                                	displayWinner(boardArray[row][col]);
+                                        return true;
                                 }
                         }
                 }
@@ -200,8 +250,10 @@ class Connect4UI extends JFrame implements ActionListener
                                 if (slot>0
                                  && slot == boardArray[row+1][col]
                                  && slot == boardArray[row+2][col]
-                                 && slot == boardArray[row+3][col])
-                                        displayWinner(g, boardArray[row][col]);
+                                 && slot == boardArray[row+3][col]){
+                               		displayWinner(boardArray[row][col]);
+                                        return true;
+                                 }
                         }
                 }
                 for (int row=0; row<MAXROW-3; row++) 
@@ -212,8 +264,10 @@ class Connect4UI extends JFrame implements ActionListener
                                 if (slot>0
                                  && slot == boardArray[row+1][col+1]
                                  && slot == boardArray[row+2][col+2]
-                                 && slot == boardArray[row+3][col+3])
-                                        displayWinner(g, boardArray[row][col]);
+                                 && slot == boardArray[row+3][col+3]){
+                               		displayWinner(boardArray[row][col]);
+                                        return true;
+                                 }
                         }
                 }
                 for (int row=MAXROW-1; row>=3; row--) 
@@ -224,33 +278,79 @@ class Connect4UI extends JFrame implements ActionListener
                                 if (slot>0
                                  && slot == boardArray[row-1][col+1]
                                  && slot == boardArray[row-2][col+2]
-                                 && slot == boardArray[row-3][col+3])
-                                        displayWinner(g, boardArray[row][col]);
+                                 && slot == boardArray[row-3][col+3]){
+                               		displayWinner(boardArray[row][col]);
+                                        return true;
+                                 }
                         }
                 }
+                return false;
+        }
+        
+        public void nextGame()
+        {
+               	if(round==7)
+               	{
+               		System.out.println("7 Rounds Finished");
+               		//Score calculation and display based on score here
+               		round = 1;
+               	        wins = 0;
+                        gameover=false;
+                        setup();
+                        repaint();
+               		return;
+               	}
+               	round++;
+                setup();
+                repaint();
+                sim = false;
         }
  
         public void actionPerformed(ActionEvent e) 
        {
                 if (e.getSource() == column1)
+                {
                         place(1);
+                }
                 else if (e.getSource() == column2)
+                {
                         place(2);
+                }
                 else if (e.getSource() == column3)
+                {
                         place(3);
+                }
                 else if (e.getSource() == column4)
+                {
                         place(4);
+                }
                 else if (e.getSource() == column5)
+                {
                         place(5);
+                }
                 else if (e.getSource() == column6)
+                {
                         place(6);
-                else if (e.getSource() == column7)
-                        place(7);
+                }
+                else if (e.getSource() == column7){
+                        if(!sim){
+                	place(7);
+                	}
+                	else
+                	{
+                		nextGame();
+                		gameover=false;
+                	}
+                }
                 else if (e.getSource() == newGame) 
                {
+               	       
+               	        round = 1;
+               	        wins = 0;
                         gameover=false;
                         setup();
                         repaint();
+                       
                 } 
                 else if (e.getSource() == exitGame) 
                {
